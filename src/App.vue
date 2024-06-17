720453a8e2edb23e7e0be93e9d7ae9b108300d7b
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2024-06-17 19:29:37 +1000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app>
+        <v-layout class="rounded rounded-md">
+            <Sidebar />
+            <v-main
+                class="d-flex align-center justify-center"
+                style="min-height: 300px"
+            >
+            </v-main>
+        </v-layout>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        <v-navigation-drawer :width="245" class="d-flex flex-column">
+            <header class="navbar-header">
+                <v-list-item title="Smart Home"></v-list-item>
+                <v-divider thickness="4"></v-divider>
+            </header>
+
+            <div class="overflow-auto">
+                <v-list-item link>Something 1</v-list-item>
+                <v-list-item link>Something 2</v-list-item>
+            </div>
+
+            <template v-slot:append>
+                <div class="pa-4">
+                    <v-btn block elevation="14" prepend-icon="mdi-cog">
+                        settings
+                    </v-btn>
+                </div>
+            </template>
+        </v-navigation-drawer>
+    </v-container>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped></style>
